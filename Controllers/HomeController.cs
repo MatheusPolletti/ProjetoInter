bfe5ad48a980afc78e4471d8decbb96626cd1951
MatheusPolletti <matheus.c.polletti@gmail.com>
2025-05-19 19:58:01 -0300

    Teste Dados Supabase

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,4 +1,5 @@
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using ProjetoInter.Data;
 
 public class HomeController : Controller
@@ -12,6 +13,49 @@ public class HomeController : Controller
 
     public async Task<IActionResult> LoginCadastro()
     {
+        // Aqui puxamos Transferencias e todas as entidades relacionadas via Include
+        var transferencias = await _context.Transferencias
+            .Include(t => t.Animal)
+                .ThenInclude(a => a.Status)
+            .Include(t => t.Animal)
+                .ThenInclude(a => a.Especie)
+            .Include(t => t.Animal)
+                .ThenInclude(a => a.Setor)
+            .Include(t => t.InstituicaoOrigem)
+            .Include(t => t.InstituicaoDestino)
+            .ToListAsync();
+
+        // Além disso, para puxar tudo, podemos trazer também outras tabelas:
+        var animais = await _context.Animais
+            .Include(a => a.Status)
+            .Include(a => a.Especie)
+            .Include(a => a.Setor)
+            .ToListAsync();
+
+        var funcionarios = await _context.Funcionarios.ToListAsync();
+        var procedimentos = await _context.Procedimentos
+            .Include(p => p.Animal)
+            .Include(p => p.FuncionarioTarefa)
+            .ToListAsync();
+
+        var setores = await _context.Setores
+            .Include(s => s.InstituicaoPertence)
+            .ToListAsync();
+
+        var atendimentos = await _context.AtendimentosVeterinarios
+            .Include(av => av.Animal)
+            .Include(av => av.FuncionarioSolicitante)
+            .Include(av => av.FuncionarioVeterinario)
+            .ToListAsync();
+
+        // Pode criar um ViewModel para agregar tudo, mas para teste pode enviar via ViewBag
+        ViewBag.Transferencias = transferencias;
+        ViewBag.Animais = animais;
+        ViewBag.Funcionarios = funcionarios;
+        ViewBag.Procedimentos = procedimentos;
+        ViewBag.Setores = setores;
+        ViewBag.Atendimentos = atendimentos;
+
         return View();
     }
 }
