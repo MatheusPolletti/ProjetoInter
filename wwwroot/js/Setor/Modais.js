6b1a5fa4e8f16115b89852afbf277d21f86bedab
Vzinha <vitoriaasssis@gmail.com>
2025-05-30 00:14:19 -0300

    Atualizações Modal

---
+++ wwwroot/js/Setor/Modais.js
@@ -0,0 +1,37 @@
+function modalNovo()
+{
+    const modalNovo = document.querySelector("#area-modal-setores");
+    const main = document.querySelector(".MainContent");
+    const overlay = document.createElement("div");
+    overlay.className = "overlay";
+    main.appendChild(overlay);
+    modalNovo.style.display = "flex";
+    modalNovo.style.position = "absolute";
+}
+function fechaModal()
+{
+    const modalNovo = document.querySelector("#area-modal-setores");
+    const main = document.querySelector(".MainContent");
+    const overlay = document.querySelector(".overlay");
+    main.removeChild(overlay);
+    modalNovo.style.display = "none";
+}
+
+function modalEditar()
+{
+    const modalNovo = document.querySelector("#area-modal-setores");
+    const main = document.querySelector(".MainContent");
+    const overlay = document.createElement("div");
+    overlay.className = "overlay";
+    main.appendChild(overlay);
+    modalNovo.style.display = "flex";
+    modalNovo.style.position = "absolute";
+}
+function modalEditar()
+{
+    const modalNovo = document.querySelector("#area-modal-setores");
+    const main = document.querySelector(".MainContent");
+    const overlay = document.querySelector(".overlay");
+    main.removeChild(overlay);
+    modalNovo.style.display = "none";
+}
